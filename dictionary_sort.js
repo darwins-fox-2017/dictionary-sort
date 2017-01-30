2dff4c36f3660d143b56c1912d997014739ffa78
Windi <windi@hacktiv8.com>
2017-01-30 16:12:50 +0700

    Template

---
+++ dictionary_sort.js
@@ -0,0 +1,7 @@
+class Dictionary {
+  // Your code here to receive user input & sort the array
+
+}
+
+const readline = require('readline');
+// your code here to initialize the program and take user input
