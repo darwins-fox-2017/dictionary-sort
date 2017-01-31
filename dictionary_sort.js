b885f34647cecb33a9960b3fb4eee1d492f86c32
Eri Irawan <eryirawan91@gmail.com>
2017-01-31 14:39:29 +0700

    all release done

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,7 +1,39 @@
-class Dictionary {
-  // Your code here to receive user input & sort the array
+"use strict"
+const readline = require('readline')
+const rl       = readline.createInterface({
+  input:process.stdin,
+  output:process.stdout,
+  prompt:'Input>'
+});
 
+rl.prompt();
+
+var arrResult = []
+
+rl.on('line', (answer)=>{
+  rl.prompt()
+  if(answer == ""){
+    rl.close()
+    console.log(arrResult);
+    console.log(dicSort(arrResult));
+  }else {
+    arrResult.push(answer)
+  }
+})
+
+function dicSort(words){
+  let tempResult = ""
+
+  for(let i = 0; i < words.length; i++){
+    for(let j = 0; j < words.length; j++){
+      if(words[i].toLowerCase() < words[j].toLowerCase()){
+        tempResult = words[j]
+        // console.log(tempResult);
+        words[j]   = words[i]
+        // console.log(words[j]);
+        words[i]   = tempResult
+      }
+    }
+  }
+  return words
 }
-
-const readline = require('readline');
-// your code here to initialize the program and take user input
