e6c99b594cfdd333e8711b7cac25f945423c8cad
Diky Arga <dikyarga.id@gmail.com>
2017-01-31 14:42:04 +0700

    final release - dictionary sort 1

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -2,7 +2,7 @@ const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  prompt: 'OHAI> '
+  prompt: 'Type a word > '
 });
 
 rl.prompt();
@@ -10,8 +10,26 @@ rl.prompt();
 var dictionaryWords = []
 
 rl.on('line', (line) => {
-  dictionaryWords.push(line)
-  console.log(dictionaryWords);
+  if (line == '') {
+    var len = dictionaryWords.length
+    console.log('Congratulations! Your dictionary has ' + dictionaryWords.length + ' words :');
+    for (var i = 0; i < len; i++) {
+      // Masukin ke var sementara [diky - 0]
+      var temp = dictionaryWords[i]
+      // console.log('nilai temp : ' + temp);
+      for (var j = i - 1; j >= 0 && dictionaryWords[j] > temp; j--) {
+        // console.log('nilai j : ' + j + ' nilai dic' + dictionaryWords[j]);
+        dictionaryWords[j + 1] = dictionaryWords[j]
+        // console.log('lalu : ' + dictionaryWords[j + 1] + ' j ' + j);
+      }
+      dictionaryWords[j + 1] = temp
+    }
+    console.log(dictionaryWords);
+  } else {
+    rl.setPrompt('Type another word (or press enter to finish : >')
+    dictionaryWords.push(line)
+    // console.log(dictionaryWords);
+  }
   rl.prompt();
 }).on('close', () => {
   console.log('Have a great day!');
