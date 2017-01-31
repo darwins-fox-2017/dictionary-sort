9f32e4d0da84f09dc318037116a948eca9147781
pisanggoreng <7ofpentacles@gmail.com>
2017-01-31 19:17:31 +0700

    endy

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,7 +1,37 @@
-class Dictionary {
-  // Your code here to receive user input & sort the array
-
-}
-
 const readline = require('readline');
-// your code here to initialize the program and take user input
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.setPrompt("mau pesan apa mas?? ");
+rl.prompt();
+  let arr = [];
+  let sort = [];
+
+rl.on('line', (words) => {
+  // kode untuk mengakhiri readline
+  if (!words) {
+    for ( var i = 0; i < arr.length; i++) {
+      let smallest = i;
+
+      for ( var j = i + 1; j < arr.length; j++) {
+        if (arr[j].toLowerCase() <  arr[smallest].toLowerCase()) {
+          smallest = j;
+        }
+      }
+      var temp = arr[i];
+      arr[i] = arr[smallest];
+      arr[smallest] = temp
+    }
+    console.log(String(arr));
+    rl.close()
+  } else {
+    rl.setPrompt("silahkan ketikkan makanan lain, tekan enter untuk mengakhiri ");
+    rl.prompt();
+    arr.push(words);
+  }
+
+})
+
+// .toLowerCase()
