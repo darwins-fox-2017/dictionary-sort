5b75881bf1f3c6efcf17abd026f554d07dea93b6
Samuel PH Sinaga <samuelphsinaga@gmail.com>
2017-01-31 19:11:29 +0700

    dictionary sort done

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,7 +1,37 @@
-class Dictionary {
-  // Your code here to receive user input & sort the array
-
-}
+"use strict"
 
 const readline = require('readline');
-// your code here to initialize the program and take user input
+const rl = readline.createInterface({
+  input : process.stdin,
+  output : process.stdot
+});
+
+var temp = [];
+console.log("input :");
+
+rl.on('line', (word) => {
+  if(word == ""){
+    console.log("Congratulation! your dictionary has " + temp.length + " words");
+    console.log(Dictionary(temp).join(" "));
+    rl.close()
+  }
+  else{
+    temp.push(word);
+    console.log("Type another word : ");
+  }
+})
+
+function Dictionary(words){
+  let result = ""
+
+  for(let i = 0; i < words.length; i++){
+    for(let j = 0; j < words.length; j++){
+      if(words[i].toLowerCase() < words[j].toLowerCase()){
+        result = words[j]
+        words[j]   = words[i]
+        words[i]   = result
+      }
+    }
+  }
+  return words
+}
