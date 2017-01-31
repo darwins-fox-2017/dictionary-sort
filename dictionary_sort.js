a5b620d3b7757e904373a0182782ea66727994e9
isumizumi <isumi.karina72@gmail.com>
2017-01-31 14:38:46 +0700

    Finish

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,7 +1,35 @@
-class Dictionary {
-  // Your code here to receive user input & sort the array
-
-}
-
 const readline = require('readline');
 // your code here to initialize the program and take user input
+const rl = readline.createInterface({
+  input : process.stdin,
+  output : process.stdot,
+});
+
+var arr_of_words = [];
+console.log("Type a word : ");
+
+rl.on('line', (word) => {
+
+  if(word == ""){
+    console.log("Congratulation! your dictionary has " + arr_of_words.length + " words");
+    for(var i=0; i<arr_of_words.length; i++){
+      console.log(arr_of_words[i]);
+    }
+    rl.close()
+  }
+  else{
+    arr_of_words.push(word);
+    console.log("Type another word : ");
+  }
+
+	arr_of_words.sort(function(a,b){
+		a = a.toString().toLowerCase(), b = b.toString().toLowerCase();
+	  for (var i = 0, n = Math.min(a.length, b.length); i<n && a.charAt(i) === b.charAt(i); ++i);
+	    if (i === n) return 0;
+	    return a.charAt(i) < b.charAt(i) ? -1 : 1;
+  });
+
+  // arr_of_words.sort(function(a,b){
+  //   return a.toLowerCase().localeCompare(b.toLowerCase());
+  // });
+});
