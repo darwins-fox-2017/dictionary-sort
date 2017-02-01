2a449864ab2cfd45a907afe97660b1df2ce9d3c9
Wahyuhidayatt <wahyuhdyt95@gmail.com>
2017-02-01 10:28:38 +0700

    dictionary-sort

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,7 +1,40 @@
-class Dictionary {
-  // Your code here to receive user input & sort the array
-
-}
-
 const readline = require('readline');
 // your code here to initialize the program and take user input
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'Type a word :'
+});
+
+const tampungKata = [];
+
+function sort(words) {
+    // let Upper = words.toUpperCase();
+    // let Lower = words.toLowerCase();
+    for (i = 1; i < words.length; i++) {
+        for (k = 0; k < i; k++) {
+            if (words[i].toUpperCase() < words[k].toUpperCase()) {
+                let temp = words[i]
+                words[i] = words[k]
+                words[k] = temp
+            }
+        }
+    }
+    return words
+}
+rl.prompt()
+rl.on('line', (kata) => {
+    if (kata == "") {
+        console.log("Congratulation! " + tampungKata.length + " words");
+        for (var i = 0; i < tampungKata.length; i++) {
+            console.log(tampungKata[i]);
+            rl.close()
+        }
+    } else {
+        rl.setPrompt('Type another word > ')
+        rl.prompt()
+
+        tampungKata.push(kata)
+        sort(tampungKata)
+    }
+})
