c9600958cf5f3567ffccf08c16de517664e08f25
Agindo <ramadhanagindo@gmail.com>
2017-02-03 08:56:06 +0700

    move to another computer

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -5,3 +5,15 @@ class Dictionary {
 
 const readline = require('readline');
 // your code here to initialize the program and take user input
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What do you think of Node.js? ', (answer) => {
+  // TODO: Log the answer in a database
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+  rl.close();
+});
