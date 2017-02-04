52ab7cdb0672b39f5c033ef96608ae98c28244f7
triyanarief <triyanarief@icloud.com>
2017-02-05 03:42:57 +0700

    done sort

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,7 +1,50 @@
-class Dictionary {
-  // Your code here to receive user input & sort the array
-
-}
+'use strict'
 
 const readline = require('readline');
 // your code here to initialize the program and take user input
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.setPrompt("Type a word : ");
+
+
+
+var dictionaryArr = [];
+function dictionaryList(inside) {
+  dictionaryArr.push(inside)
+}
+
+function dictionarySort(arr) {
+  var tukar;
+    do {
+        tukar = false;
+        for (var i=0; i < arr.length-1; i++) {
+            if (arr[i].toLowerCase() > arr[i+1].toLowerCase()) {
+                let temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+                tukar = true;
+            }
+        }
+    } while (tukar);
+  return arr;
+}
+
+let i=0;
+// dictionarySort(dictionaryArr)
+rl.prompt();
+rl.on('line', (inputs) => {
+  dictionaryList(inputs)
+  rl.prompt();
+  i++
+})
+rl.on('close', (a) => {
+  rl.setPrompt("");
+  console.log("Congratulations! Your dictionary has " + i + " words")
+  let dictionaryArrSorted = dictionarySort(dictionaryArr)
+  dictionaryArrSorted.forEach(function(isi) {
+    console.log(isi);
+  })
+})
